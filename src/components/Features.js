14ee22fedd4b98666a767bf904fb1f7b9abc34e2
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-10-28 10:38:11 +0100

    Create Features.js

---
+++ src/components/Features.js
@@ -0,0 +1,18 @@
+export default function Features() {
+return (
+<div className="features">
+<div className="feature-card">
+<h3>ReadingRewards</h3>
+<p>Join now to earn FREE BOOKS</p>
+</div>
+<div className="feature-card">
+<h3>LovedBooks Deals</h3>
+<p>save up to 20% off any eligible item</p>
+</div>
+<div className="feature-card">
+<h3>Mobile App</h3>
+<p>Shop faster & earn bonus points</p>
+</div>
+</div>
+)
+}
