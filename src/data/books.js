f97a94f1bd0f34982681481381be27c216e39ba4
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-10-28 10:34:37 +0100

    Create books.js

---
+++ src/data/books.js
@@ -0,0 +1,104 @@
+export const topSellers = [
+{
+id:1,
+title:"The Hobbit",
+author:"J.R.R. Tolkien",
+price:"from $4.19",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+},
+{
+id:2,
+title:"Little Women",
+author:"Louisa May Alcott",
+price:"from $3.99",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+},
+{
+id:3,
+title:"Crime and Punishment",
+author:"Fyodor Dostoevsky",
+price:"from $5.69",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+},
+{
+id:4,
+title:"Sharp Objects",
+author:"Gillian Flynn",
+price:"from $4.19",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+}
+]
+export const newTrending = [
+{
+id:1,
+title:"How to Test Negative for Stupid",
+author:"John Kennedy",
+price:"from $22.59",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+},
+{
+id:2,
+title:"Nobody's Girl",
+author:"Virginia Roberts Guifre",
+price:"from $25.98",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+},
+{
+id:3,
+title:"Alchemised",
+author:"Seq LinVu",
+price:"from $27.47",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+},
+{
+id:4,
+title:"Partypooper",
+author:"Jeff Kinney",
+price:"from $13.25",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+}
+]
+export const deals = [
+{
+id:1,
+title:"Night",
+author:"Elie Wiesel",
+price:"from $4.19",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+},
+{
+id:2,
+title:"The Grapes of Wrath",
+author:"John Steinbeck",
+price:"from $5.19",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+},
+{
+id:3,
+title:"Matilda",
+author:"Roald Dahl",
+price:"from $3.59",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+},
+{
+id:4,
+title:"Redeeming Love",
+author:"Francine Rivers",
+price:"from $4.49",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+},
+{
+id:5,
+title:"The Hate U Give",
+author:"Angie Thomas",
+price:"from $4.29",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761640628/tse7iox1uvvvb9txshq9.jpg"
+},
+{
+id:6,
+title:"A Tale of Two Cities",
+author:"Charles Dickens",
+price:"from $3.79",
+image:"https://res.cloudinary.com/dqhawdcol/image/upload/v1761641455/ekeuarcoiun6gocactrs.jpg"
+}
+]
