fcaae2248d31b875bebade5a4b19a01e49349520
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-10-28 10:39:28 +0100

    Create ReadingSection.js

---
+++ src/components/ReadingSection.js
@@ -0,0 +1,27 @@
+export default function ReadingSection() {
+return (
+<div className="reading-section">
+<h2 className="reading-title">For the Love of Reading</h2>
+<div className="reading-content">
+<div className="reading-text">
+<div className="reading-features">
+<div className="reading-feature">
+<h3>Selection</h3>
+<p>We have more than 15 million titles to choose from, from the earliest board books to the all-time classics of literature.</p>
+</div>
+<div className="reading-feature">
+<h3>Purchasing Power</h3>
+<p>Used books are often treasures that are out-of-print or rare. With Wish Lists you can choose to be notified the instant we find a copy, see how often we find rare titles, and see who else is interested.</p>
+</div>
+<div className="reading-feature">
+<h3>FREE Shipping & More</h3>
+<p>When you've found the books you want we'll ship qualifying orders to your door for FREE in 100% recyclable packaging. If there is no demand for a book, we will donate it to charity, or we'll recycle it.</p>
+</div>
+</div>
+<a href="/about" className="more-about-btn">More About LovedBooks</a>
+</div>
+<div className="reading-image"></div>
+</div>
+</div>
+)
+}
