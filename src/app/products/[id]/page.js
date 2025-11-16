09113e7111eec495b4bc94d7c4c9d3fcb5f94f17
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-16 17:14:06 +0100

    Create page.js

---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,14 @@
+'use client'
+import Header from '../../../components/Header'
+import Footer from '../../../components/Footer'
+import ProductPage from '../../../components/ProductPage'
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <ProductPage />
+      <Footer />
+    </>
+  )
+}
