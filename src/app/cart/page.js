5edd3139ca52f50b1e2c5b1fe924dc0f4d74dee2
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-16 17:59:28 +0100

    Add cart page component with header and footer

---
+++ src/app/cart/page.js
@@ -0,0 +1,14 @@
+'use client'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import CartPage from '../../components/CartPage'
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <CartPage />
+      <Footer />
+    </>
+  )
+}
