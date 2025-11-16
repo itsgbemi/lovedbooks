86289991c2b98cf5273766d4e1bb74bde7c05368
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-16 16:26:27 +0100

    Create BookGrid.js

---
+++ src/components/BookGrid.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+export default function BookGrid({ books }) {
+  return (
+    <div className="books-grid">
+      {books.map(book => (
+        <Link key={book.id} href={`/products/${book.id}`} className="book-card">
+          <div className="book-image" style={{backgroundImage: `url(${book.image})`}}></div>
+          <div className="book-info">
+            <div className="book-title">{book.title}</div>
+            <div className="book-author">{book.author}</div>
+            <div className="book-price">{book.price}</div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
